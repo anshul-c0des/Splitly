270c5d8e641c364246fbb8f79a578ef24d209c9e
anshul_c0des <anshuldhariya5@gmail.com>
2025-07-10 17:03:14 +0530

    contacts page created

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({subsets: ['latin']});
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
             <Header />
             <main className="min-h-screen">
               {children}
+              <Toaster richColors />
             </main>
           </ConvexClientProvider>
         </ClerkProvider>
